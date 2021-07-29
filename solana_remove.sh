4a56c45ae12fb71971bbfab1daebf64689931af5
pavlo-maiorchyk <85483625+pavlo-maiorchyk@users.noreply.github.com>
2021-07-29 18:04:07 +0300

    Solana remove script

---
+++ solana_remove.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Removing Solana ..."
+
+# Stop solana
+systemctl stop solana
+
+# Disable solana on startup
+systemctl disable solana
+
+# Remove service file from systemd
+rm /etc/systemd/system/solana.service
+
+# Reload daemon
+systemctl daemon-reload
+
+# Remove solana folders
+rm -r /root/solana
+rm -r /root/solana_update
+rm -r /root/solanamonitoring
+rm -r /root/.local/share/solana
+
+# Remove Zabbix
+rm /root/nodemon.sh
+rm /root/slotBehind.py
+rm /root/zabbix-release*
+systemctl stop zabbix 
+systemctl disable zabbix 
+apt-get --purge remove -y zabbix-*
